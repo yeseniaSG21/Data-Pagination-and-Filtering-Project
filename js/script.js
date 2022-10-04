99a0ea41c00455927d98b01c04f3a899a2586f7d
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-10-04 15:37:21 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,23 +1,27 @@
-/*
+/*********************************************
 Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
-*/
+**********************************************/
 
+/**
+* This function will create and insert/append the elements needed to display a "page" of nine students
+  * @param {sting} list - the array of student data from data.js
+  * @param {number} page - the page number we want to display
+**/
+function showPage (list, page) {
+  // create two variables which will represent the index for the first and last student on the page
 
+    // select the element with a class of `student-list` and assign it to a variable
 
-/*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
-*/
+    // set the innerHTML property of the variable you just created to an empty string
 
-
-
-/*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
-*/
-
+    // loop over the length of the `list` parameter
+      // inside the loop create a conditional to display the proper students
+        // inside the conditional:
+          // create the elements needed to display the student information
+          // insert the above elements
+}
+showPage(data, 1);
 
 
 /*
