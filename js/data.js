f425167b47b4d604ce893fd6b0fed7204c942159
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-10-05 11:29:25 -0700

    Update script.js

--- js/data.js
+++ js/data.js
@@ -3,7 +3,7 @@ Treehouse Techdegree:
 FSJS Project 2 - Data Pagination and Filtering
 */
 
-// Array of student objects
+//Array of student objects
 const data = [
   {
     name: {
--- js/script.js
+++ js/script.js
@@ -14,6 +14,11 @@ function showPage (list, page) {
   const studentList = document.querySelector('.student-list');
   studentList.innerHTML = '';
 
+  for ( let i = 0; i < list.length; i++ ){
+    if ( i >= startIndex && i < endIndex ) {
+      
+    }
+  }
     // loop over the length of the `list` parameter
       // inside the loop create a conditional to display the proper students
         // inside the conditional:
@@ -27,8 +32,23 @@ showPage(data, 1);
   * @param {string} list - the array of student data from data.js
 **/
 function addPagination (list) {
+  // create a variable to calculate the number of pages needed
 
+    // select the element with a class of `link-list` and assign it to a variable
 
+    // set the innerHTML property of the variable you just created to an empty string
+
+    // loop over the number of pages needed
+      // create the elements needed to display the pagination button
+      // insert the above elements
+
+    // give the first pagination button a class of "active"
+
+    // create an event listener on the `link-list` element
+      // if the click target is a button:
+        // remove the "active" class from the previous button
+        // add the active class to the clicked button
+        // call the showPage function passing the `list` parameter and page to display as arguments
 }
 
 // Call functions
