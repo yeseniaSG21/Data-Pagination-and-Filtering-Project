f9340479f5746e079f4536b9fafc2a79da30ccf6
Yesenia Sosa Gonzalez <gonzalez.yesenia0621@gmail.com>
2022-10-05 12:14:57 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,22 +16,27 @@ function showPage (list, page) {
 
   for ( let i = 0; i < list.length; i++ ){
     if ( i >= startIndex && i < endIndex ) {
-      
+      let studentItem =
+          ` <li class="student-item cf">
+              <div class="student-details">
+                <img class="avatar" src="${list[i].picture.large}" alt="Profile Picture">
+                <h3>${list[i].name.title}. ${list[i].name.first} ${list[i].name.last}</h3>
+                <span class="email">${list[i].email}</span>
+              </div>
+              <div class="joined-details">
+                <span class="date">Joined: ${list[i].registered.date}</span>
+              </div>
+              </li> `;
+      studentList.insertAdjacentHTML('beforeend', studentItem);
     }
   }
-    // loop over the length of the `list` parameter
-      // inside the loop create a conditional to display the proper students
-        // inside the conditional:
-          // create the elements needed to display the student information
-          // insert the above elements
 }
-showPage(data, 1);
 
 /**
 *This function will create and insert/append the elements needed for the pagination buttons
   * @param {string} list - the array of student data from data.js
 **/
-function addPagination (list) {
+//function addPagination (list) {
   // create a variable to calculate the number of pages needed
 
     // select the element with a class of `link-list` and assign it to a variable
@@ -49,6 +54,7 @@ function addPagination (list) {
         // remove the "active" class from the previous button
         // add the active class to the clicked button
         // call the showPage function passing the `list` parameter and page to display as arguments
-}
+//}
 
 // Call functions
+showPage(data, 1);
